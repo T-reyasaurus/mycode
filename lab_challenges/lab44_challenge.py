9de5348cf1bb68c080d3700d533f86d982a10f85
T-reyasaurus <stegmantreycropper@gmail.com>
2023-04-21 22:35:51 +0000

    Outputting files

--- lab_challenges/lab44_challenge.py
+++ lab_challenges/lab44_challenge.py
@@ -1,30 +1,23 @@
 #!/usr/bin/python3
-"""Lab Challenge: Range Practice"""
-
-
-
+"""Lab Challenge 44"""
 def main():
-   
-    while True:
+    done = False
+    while not done:
         try:
             beer = int(input("How many beer on the wall? "))
             for i in range(beer, 0, -1):
                 if i > 99:
                     print(f"That's too much beer! We have to drive home! Try again")
                     break
-            
                 if i > 1:
                     print(f"{i} bottles of beer on the wall, {i} bottles of beer.Take one down, pass it around, {i-1} bottles of beer on the wall.")
-            
                 elif i == 1:
                     print(f"{i} bottle of beer on the wall, {i} bottle of beer.Take it down, pass it around, no more bottles of beer on the wall.")
-                    print("No more bottles of beer on the wall, no more bottles of beer.") 
+                    print("No more bottles of beer on the wall, no more bottles of beer.")
                     print("Go to the store and buy some more, 99 bottles of beer on the wall.")
-                
-            break
-
+                    done = True
+                    break
         except ValueError:
             print("Come on and play along! You know the song!")
-
 if __name__ == "__main__":
     main()
--- lab_challenges/lab45_challenge.py
+++ lab_challenges/lab45_challenge.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3
 """Lab45 Challenge: Farm Loops"""
+farms = [{"name": "Southwest Farm", "agriculture": ["chickens", "carrots", "celery"]},
+         {"name": "Northeast Farm", "agriculture": ["sheep", "cows", "pigs", "chickens", "llamas", "cats"]},
+         {"name": "East Farm", "agriculture": ["bananas", "apples", "oranges"]},
+         {"name": "West Farm", "agriculture": ["pigs", "chickens", "llamas"]}]
+yuck= ["carrots", "celery"]
+mmm= ["bananas", "apples", "oranges"]
 
-farms = [{"name": "NE Farm", "agriculture": ["sheep", "cows", "pigs", "chickens", "llamas", "cats"]},
-         {"name": "W Farm", "agriculture": ["pigs", "chickens", "llamas"]},
-         {"name": "SE Farm", "agriculture": ["chickens", "carrots", "celery"]}]
-NE_animals = farms[0]["agriculture"]
-#choice = input("Choose a farm (NE Farm, W Farm, or SE Farm)--> ")
+for farm in farms:
+    print("-", farm["name"])
+choice= input("Pick a farm!\n")
 
-def main():
-
-#for i in NE_animals:
-#    print(i)
-    for farm in farms:
-        print("-", farm["name"])
-    choice= input("Pick a farm!\n")
-      continue
-    
-    for farm in farms:
-        if farm["name"].lower() == choice.lower:
-             print(choice)
-if __name__ == "__main__":
-    main()
-    # you do the rest!
-
+for farm in farms:
+    if farm["name"].lower() == choice.lower():
+        print(farm)
+        
+        for ag_item in farm["agriculture"]:
+            if ag_item not in yuck:
+                 print(ag_item)
